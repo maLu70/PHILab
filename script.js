67ea024c309d22642cb0878c038e48da89d5e2ca
maLu70 <lumasiqueira06@gmail.com>
2024-09-22 19:31:13 -0300

    .

---
+++ script.js
@@ -0,0 +1,43 @@
+function bcp() {
+    document.getElementById("bcp").style.color = "#1C3C61";
+    document.getElementById("bch").style.color = "black";
+    document.getElementById("bmf").style.color = "black";
+
+    document.getElementById("bcp").style.fontWeight = "bold";
+    document.getElementById("bch").style.fontWeight = "normal";
+    document.getElementById("bmf").style.fontWeight = "normal";
+
+    document.querySelector(".infoBCP").style.display = "block";
+    document.querySelector(".infoBCH").style.display = "none";
+    document.querySelector(".infoBMF").style.display = "none";
+}
+
+function bch() {
+    document.getElementById("bch").style.color = "#1C3C61";
+    document.getElementById("bcp").style.color = "black";
+    document.getElementById("bmf").style.color = "black";
+
+    document.getElementById("bcp").style.fontWeight = "normal";
+    document.getElementById("bch").style.fontWeight = "bold";
+    document.getElementById("bmf").style.fontWeight = "normal";
+
+    document.querySelector(".infoBCP").style.display = "none";
+    document.querySelector(".infoBCH").style.display = "block";
+    document.querySelector(".infoBMF").style.display = "none";
+
+}
+
+function bmf() {
+    document.getElementById("bmf").style.color = "#1C3C61";
+    document.getElementById("bcp").style.color = "black";
+    document.getElementById("bch").style.color = "black";
+    
+    document.getElementById("bcp").style.fontWeight = "normal";
+    document.getElementById("bch").style.fontWeight = "normal";
+    document.getElementById("bmf").style.fontWeight = "bold";
+
+    document.querySelector(".infoBCP").style.display = "none";
+    document.querySelector(".infoBCH").style.display = "none";
+    document.querySelector(".infoBMF").style.display = "block";
+
+}
